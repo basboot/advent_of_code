bfa5b1207ddc812f6a2d85ddbbe7285ccebcb549
Bas Boot <github@basboot.nl>
2023-12-05 22:40:03 +0100

    Add comments

--- 2023/q3a.py
+++ 2023/q3a.py
@@ -33,77 +33,67 @@ def next_to_gears(position):
                 gears.append((position[0] + i, position[1] + j))
     return gears
 
+
 map = []
-
-
-i = 0
 for line in lines:
     row = line.rstrip()
-    # print("input: ", row)
-
-    map_row = []
-    j = 0
-    for c in row:
-        map_row.append(c)
-
-        j += 1
+    map_row = [c for c in row]
     map_row.append('.') # add extra dot as EOL
     map.append(map_row)
-    i += 1
 
-# find numbers
-in_number = False
-number_str = ""
-is_part = False
-next_gears = []
-gears = {}
+
+# find numbers by looping through the map
+in_number = False # currently in a number?
+number_str = "" # to build a number from multiple digits
+is_part = False # is current number a partnumber
+next_gears = [] # which gears are next to the current number
+gears = {} # dict with gear to numbers
 
 total = 0
-
 for i in range(len(map)):
     for j in range(len(map[0])):
         map_cell = map[i][j]
         if in_number:
             if map_cell.isnumeric():
-                number_str = number_str + map_cell
-                is_part = is_part or is_partnumber((i, j))
-                next_gears = next_gears + next_to_gears((i, j))
+                number_str = number_str + map_cell # build number
+                is_part = is_part or is_partnumber((i, j)) # if a symbol has been found it stays a partnumber
+                next_gears = next_gears + next_to_gears((i, j)) # store all 'gears' for this number
             else:
-                # print(f"number found {number_str}, part {is_part}, gears {next_gears}")
+                # only partnumbers add to total
                 if is_part:
                     total += int(number_str)
 
+                # all numbers can be gears
                 for gear in next_gears:
-                    # print(gear)
                     if gear in gears:
-                        gears[gear].add(int(number_str))
+                        gears[gear].add(int(number_str)) # add number to gear...
                     else:
-                        gears[gear] = {int(number_str)}
+                        gears[gear] = {int(number_str)} # ...or create new gear in dict for this number
+
+                # reset values for next number
                 number_str = ""
                 in_number = False
                 is_part = False
                 next_gears = []
         else:
+            # find start of new number
             if map_cell.isnumeric():
                 number_str = map_cell
                 in_number = True
                 is_part = is_partnumber((i, j))
                 next_gears = next_gears + next_to_gears((i, j))
 
-
-
-# print(map)
 print("Part 1", total)
 
-# 595217 too high
 
 total_gear_ratios = 0
 for gear in gears:
-    # print(gears[gear])
-    if len(gears[gear]) == 2:
-        # print("found", gears[gear])
+    if len(gears[gear]) == 2: # only count gears with two numbers
         g = list(gears[gear])
         ratio = g[0] * g[1]
         total_gear_ratios += ratio
 
 print("Part 2", total_gear_ratios)
+
+# Part 1 514969
+# Part 2 78915902
