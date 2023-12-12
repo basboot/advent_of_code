4db91ef458e8f6ef6b78a7d519466aa2153dd1b4
Bas Boot <github@basboot.nl>
2023-12-12 22:14:24 +0100

    Day 12 part 1, really increase speed

--- 2023/q12a_new.py
+++ 2023/q12a_new.py
@@ -1,5 +1,6 @@
 # Using readlines()
 import math
+import time
 
 file1 = open('q12a.txt', 'r')
 lines = file1.readlines()
@@ -7,33 +8,23 @@ from ast import literal_eval
 
 # operational (.) or damaged (#) or ?
 
+max_spring_length = -math.inf
+
 springs = []
 for line in lines:
     unknown, known = list(line.rstrip().split(" ")[0]), literal_eval("[" + line.rstrip().split(" ")[1] + "]")
     springs.append((unknown, known))
+    max_spring_length = max(max_spring_length, max(known))
 
-# count neighbouring #
-def analyse_arrangement(current_arrangement):
-    arrangement = []
-    hash_count = 0
-    for spring in current_arrangement:
-        match spring:
-            case "#":
-                hash_count += 1
-            case ".":
-                if hash_count > 0:
-                    arrangement.append(hash_count)
-                    hash_count = 0
-            case _:
-                assert True, "Arrangement can only contain # and ."
 
-    if hash_count > 0:
-        arrangement.append(hash_count)
-
-    return arrangement
+start_time = time.time()
 
 # start of spring, spring length, array #.?
 def is_legal(pos, spring, unknown):
+    if spring == 0:
+        if unknown[pos] == "?" or unknown[pos] ==".":
+            return True
+
     # can position before be .
     if pos > 0 and unknown[pos - 1] == "#":
         return False
@@ -54,162 +45,64 @@ def is_legal(pos, spring, unknown):
     # everything is ok :-)
     return True
 
-# find lower and upper bound to fit a spring (left to right), starting from pos
-def find_fits(pos, spring, unknown):
-    first_legal = None
-    last_legal = None
+def create_constraints(unknown):
+    constraints = []
+    for pos in range(len(unknown)):
+        constraint = []
+        # find all spring length that can be used at this position
+        for i in range(max_spring_length + 1):
+            if (is_legal(pos, i, unknown)):
+                constraint.append(i)
+        constraints.append(constraint)
 
-    # print("ff", pos, spring, unknown)
-
-    # find until where this spring can be fitted
-    for pos in range(pos, len(unknown)):
-        if is_legal(pos, spring, unknown):
-            if first_legal is None:
-                first_legal = pos
-            last_legal = pos + spring - 1
-        else:
-            # break after it fits, but does not fit anymore
-            if first_legal is not None:
-                break
-
-    assert first_legal is not None and last_legal is not None, "assumption that all springs fit does not hold"
-
-    return first_legal, last_legal
-#
-# def break_puzzle(unknown, known):
-#     unknowns = []
-#     knowns = []
-#
-#     start_search = 0
-#     upper_bound = len(unknown) - 1
-#     spring_id = 0
-#
-#     start_spring = 0
-#     start_unknown = 0
-#
-#     while spring_id < len(known):
-#         spring = known[spring_id]
-#
-#         # print(f"spring {spring}")
-#
-#         first_pos, last_pos = find_fit(start_search, spring, unknown)
-#
-#         # print(f"{spring} fits {first_pos} - {last_pos}")
-#
-#         if last_pos < upper_bound:
-#             upper_bound = last_pos
-#
-#         start_search = first_pos + spring + 1 # +1, because we need an empty spot behind the spring
-#
-#         # print(start_search, upper_bound)
-#
-#         spring_id += 1
-#
-#         # break
-#         if start_search > upper_bound:
-#
-#             # print(f"break {start_spring} to {spring_id - 1}")
-#             # print(f"{unknown[start_unknown:start_search]}")
-#             new_unknown = unknown[start_unknown:start_search]
-#             new_unknown[-1] = "." # we know this must be an empty space
-#             unknowns.append(new_unknown)
-#             # print(f"{known[start_spring:spring_id]}")
-#             knowns.append(known[start_spring:spring_id])
-#             upper_bound = math.inf
-#             start_spring = spring_id
-#             start_unknown = start_search
-#
-#             # print(f"SU {start_unknown}, with {start_spring}")
-#
-#     # if not all springs have been fitted, add them
-#     if len(known[start_spring:]) > 0:
-#         unknowns.append(unknown[start_unknown:])
-#         knowns.append(known[start_spring:])
-#
-#     return unknowns, knowns
+    return constraints
 
 
 
-def is_legal_arrangement(current_arrangement, known, complete=False):
-    arrangement = analyse_arrangement(current_arrangement)
+total = 0
 
-    if complete:
-        return arrangement == known
-    else:
-        return arrangement == known[:len(arrangement)]
-
-def create_legal_arrangements(current_arrangement, unknown, known, depth, legal_arrangements):
-    # check solution if we reach the end
-    if depth == len(unknown):
-        if is_legal_arrangement(current_arrangement, known, True):
-            legal_arrangements.append(current_arrangement)
+legal_arrangements = 0
+def count_legal_arrangements(constraints, known, depth):
+    global legal_arrangements
+    if depth == len(constraints): # at end
+        if len(known) == 0: # and everything used
+            legal_arrangements += 1
+        #     print(solution)
+        # else:
+        #     print("end reached without solution: ", solution)
         return
 
-    # backtrack on illegal arrangements
-    if unknown[depth] == ".":
-        if not is_legal_arrangement(current_arrangement, known):
-            return
-
-    # known characters can always be added
-    if unknown[depth] in [".", "#"]:
-        create_legal_arrangements(current_arrangement + [unknown[depth]], unknown, known, depth + 1, legal_arrangements)
+    # use config, or use 0
+    if len(known) > 0:
+        options = [0, known[0]]
     else:
-        assert unknown[depth] == "?", f"Illegal char in unknown arrangement {unknown[depth]}"
+        options = [0]
 
-        # try both options for ?
-        for spring_option in [".", "#"]:
-            create_legal_arrangements(current_arrangement + [spring_option], unknown, known, depth + 1, legal_arrangements)
+    for option in options:
+        # print(f"try {option} at depth {depth}")
+        # print(">>>", depth, constraints)
+        if option in constraints[depth]: # TODO + 1
+            count_legal_arrangements(constraints, known if option == 0 else known[1:], min(len(constraints), depth + option + 1))
 
 
-def replace_only_fit(unknown, known):
-    for i in range(len(known)):
-        spring = known[i]
 
-        count = 0
 
-        # print("------", spring)
 
-        for pos in range(len(unknown)):
-            if is_legal(pos, spring, unknown):
-                count += 1
-        if count == 1:
-            print("FOUND")
+print("Part 1", total)
+# print("max spring", max_spring_length)
 
-print(replace_only_fit(['?', '?', '.', '.', '#', '#', '#', ".", "?"], [1, 1, 3]))
+constraints = create_constraints(list(['?', '?', '.', '?']))
+# print(constraints)
+count_legal_arrangements(constraints, [1, 1], 0)
+print(legal_arrangements)
 
-# for unknown, known in springs:
-#     partial_unknowns, partial_knowns = break_puzzle(unknown, known)
+for unknown, known in springs:
+    legal_arrangements = 0
+    constraints = create_constraints(unknown)
+    count_legal_arrangements(constraints, known, 0)
+    total += legal_arrangements
 
-# legal_arrangements = []
+print("Part 1", total)
 
-# for unknown, known in springs:
-#     create_legal_arrangements([], unknown, known, 0, legal_arrangements)
-# print("Part 1", len(legal_arrangements))
+print("--- %s seconds ---" % (time.time() - start_time))
 
-# print(springs[0][0], springs[0][1])
-# print(break_puzzle(['?', '?', '?', '.', '#', '#', '#', "?", "?"], springs[0][1]))
-
-# total = 0
-# for unknown, known in springs:
-#     partial_unknowns, partial_knowns = break_puzzle(unknown, known)
-#
-#     print("---------")
-#     print(unknown, known)
-#
-#     n_arrangements = 1
-#     for i in range(len(partial_unknowns)):
-#         legal_arrangements = []
-#         create_legal_arrangements([], partial_unknowns[i], partial_knowns[i], 0, legal_arrangements)
-#         n_arrangements *= len(legal_arrangements)
-#
-#         print(partial_unknowns[i], partial_knowns[i])
-#         print(len(legal_arrangements))
-#
-#     print("-", n_arrangements)
-#
-#     break
-#     total += n_arrangements
-#
-#
-#
-# print("Part 1", total)
