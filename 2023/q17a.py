044440d861f8a841d9de8e8788f637bc271518ed
Bas Boot <github@basboot.nl>
2024-01-08 20:30:53 +0100

    Fix after advent tools update

--- 2023/q17a.py
+++ 2023/q17a.py
@@ -12,7 +12,7 @@ sys.setrecursionlimit(10000)
 file1 = open('q17a.txt', 'r')
 lines = file1.readlines()
 
-city, width, height = read_grid(lines, GRID_NUMPY, lambda a: a.strip())
+city, width, height = read_grid(lines, GRID_NUMPY, lambda a: a.strip(), value_conversions=None, int_conversion=True)
 
 
 MAX_STRAIGHT = 10 #Part 1, 3
