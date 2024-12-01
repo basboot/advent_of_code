ebd84cf22376710f040a089c1fc4317c708a270f
Bas Boot <github@basboot.nl>
2024-12-01 15:01:51 +0100

    Fix cleaner solution

--- 2024/q1a.py
+++ 2024/q1a.py
@@ -5,7 +5,7 @@ import numpy.typing as npt
 lists: npt.NDArray[int] = np.sort(np.loadtxt('q1a.txt', dtype=int), 0)
 
 # sum up distances between numbers in lists
-total: int = np.sum(np.diff(lists))
+total: int = np.sum(np.abs(np.diff(lists)))
 
 
 print(f"Part 1, sum of distances = {total}")
