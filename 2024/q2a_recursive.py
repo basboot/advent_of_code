17c4831821811e9ec7ded446f66c548d520726a5
Bas Boot <github@basboot.nl>
2024-12-02 16:17:24 +0100

    Cleanup

--- 2024/q2a_recursive.py
+++ 2024/q2a_recursive.py
@@ -7,7 +7,7 @@ file1 = open('q2a.txt', 'r')
 lines: [str] = file1.readlines()
 
 def count_errors(current: int, report: [int]) -> int:
-    # end of report
+    # end of report, so no more errors
     if current >= len(report):
         return 0
 
@@ -17,6 +17,8 @@ def count_errors(current: int, report: [int]) -> int:
     if current == 0 or (0 < abs(report[current] - report[current - 1]) < 4 and (current == 1 or np.sign(report[current] - report[current - 1]) == np.sign(report[current - 1] - report[current - 2]))):
         return count_errors(current + 1, report)
     else:
+        # if not safe, problem can be this number, the previous number, and even the one before (if it is the first it might change inc/dec)
+        # try all three, and use best
         return 1 + min(count_errors(0, [report[i] for i in range(len(report)) if i != current]), count_errors(0, [report[i] for i in range(len(report)) if i != current - 1]) if current > 0 else math.inf, count_errors(0, [report[i] for i in range(len(report)) if i != current - 2]) if current > 1 else math.inf)
 
 errors = defaultdict(int)
