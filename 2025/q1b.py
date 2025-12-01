32488ae666c4e64938bd0d3ee7d2ce6113b47f48
Bas Boot <github@basboot.nl>
2025-12-01 16:55:07 +0100

    Fix q1

--- 2025/q1b.py
+++ 2025/q1b.py
@@ -1,32 +1,46 @@
 def modulus(n):
+    result = n
+
     zeros = 0
 
-    while n < 0:
+    while result < 0:
         zeros += 1
-        n += 100
+        result += 100
 
-    while n > 99:
+    while result > 99:
         zeros += 1
-        n -= 100
+        result -= 100
 
-    return n, zeros
+    return result, zeros
 
 
 current = 50
 zeros = 0
+
+max_number = 0
 
 with open("q1a.txt") as f:
     for line in f:
         code = line.strip()
         direction, number = code[0], int(code[1:])
 
+        if current == 0 and direction == "L":
+            zeros -= 1
+
         current, extra_zeros = modulus(current + number * (1 if direction == "R" else -1))
 
         zeros += extra_zeros
+
+        if current == 0 and direction == "L":
+            zeros += 1
+
+        print(direction, number, zeros)
+
+print("last: ", current)
 
 print(f"Part 2: {zeros}")
 
 # 6294 too high
 
 
-print(modulus(50 + 50))
+# print(modulus(0 + -1))
