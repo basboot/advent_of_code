29f49cf3cd70adc18b103233597117c1e0e388b8
Bas Boot <github@basboot.nl>
2025-12-01 17:35:08 +0100

    Cleanup

--- 2025/q1a2.py
+++ 2025/q1a2.py
@@ -8,7 +8,7 @@ with open("q1a.txt") as f:
         direction, offset, rounds = 1 if code[0] == "R" else -1, int(code[1:]) % 100, int(code[1:]) // 100
         offset *= direction
 
-        # rounds always add zeros, going over 0 only when not coming from 0
+        # rounds always add zeros, going over 0 only adds a zero when not coming from 0
         extra_zeros += rounds + (current + offset > 100 or current + offset < 0 if (current > 0) else 0)
         current += offset
         current %= 100
