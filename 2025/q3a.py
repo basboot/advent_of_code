613fbc0bf06135b49a378159b4206ed5b6a4a7b8
Bas Boot <github@basboot.nl>
2025-12-03 08:06:17 +0100

    Add q3b

--- 2025/q3a.py
+++ 2025/q3a.py
@@ -1,23 +1,21 @@
-joltage = 0
+joltage1, joltage2 = 0, 0
+
+def find_max_joltage(batteries, n):
+    battery_joltage = max(batteries[0:len(batteries) - n])
+    if n == 0:
+        return battery_joltage
+    else:
+        battery_index = batteries.index(battery_joltage)
+        return battery_joltage * 10**n + find_max_joltage(batteries[battery_index + 1:], n - 1)
+
 
 with open("q3a.txt") as f:
     for line in f:
         batteries = list(map(int, list(line.strip())))
-        left = max(batteries)
 
-        # corner case, largest is the last
-        left_idx = batteries.index(left)
+        joltage1 += find_max_joltage(batteries, 1) # zero indexed, so 2 batteries = 1
+        joltage2 += find_max_joltage(batteries, 11)  # zero indexed, so 2 batteries = 1
 
-        if left_idx == len(batteries) - 1:
-            right = left
-            left = max(batteries[0:left_idx])
-        else:
-            right = max(batteries[left_idx + 1:])
-
-        print(left, right)
-
-        joltage += 10 * left + right
-
-
-print(f"Part 1: {joltage}")
+print(f"Part 1: {joltage1}")
+print(f"Part 2: {joltage2}")
 
